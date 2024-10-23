e3abce59b5f3dfce112a410cad62f138b5be59d5
josuedor <josuedor@gmail.com>
2024-10-23 12:25:49 -0500

    Add Parse SDK and configuration, implement login

--- src/app/auth/authForms/AuthLogin.jsx
+++ src/app/auth/authForms/AuthLogin.jsx
@@ -8,83 +8,90 @@ import Link from 'next/link';
 import CustomCheckbox from '@/app/components/forms/theme-elements/CustomCheckbox';
 import CustomTextField from '@/app/components/forms/theme-elements/CustomTextField';
 import CustomFormLabel from '@/app/components/forms/theme-elements/CustomFormLabel';
+import Parse from '../../../utils/parse';
 
+const AuthLogin = ({ title, subtitle, subtext }) => {
 
-const AuthLogin = ({ title, subtitle, subtext }) => (
-  <>
-    {title ? (
-      <Typography fontWeight="700" variant="h3" mb={1}>
-        {title}
-      </Typography>
-    ) : null}
+  const handleLogin = async () => {
+    await Parse.User.logIn('admin', 'secret');
+    const loggedIn = Parse.User.current();
+    console.log(loggedIn);
+  };
 
-    {subtext}
-
-
-
-    <Stack>
-      <Box className="muitech">
-        <CustomFormLabel className="nametech" htmlFor="username">Username</CustomFormLabel>
-        <CustomTextField id="username" placeholder="Username" variant="outlined" fullWidth />
-      </Box>
-      <Box className="muitech">
-        <CustomFormLabel className="nametech" htmlFor="password">Password</CustomFormLabel>
-        <CustomTextField id="password" placeholder="password" type="password" variant="outlined" fullWidth />
-      </Box>
-      <Stack justifyContent="space-between" direction="row" alignItems="center" my={2}>
-        <FormGroup>
-          <FormControlLabel
-            control={<CustomCheckbox defaultChecked />}
-            label="Remeber this Device"
-          />
-        </FormGroup>
-        <Typography
-          component={Link}
-          href="/auth/auth1/forgot-password"
-          fontWeight="500"
-          sx={{
-            textDecoration: 'none',
-            color: 'primary.main',
-          }}
-        >
-          
+  return (
+    <>
+      {title ? (
+        <Typography fontWeight="700" variant="h3" mb={1}>
+          {title}
         </Typography>
+      ) : null}
+
+      {subtext}
+
+
+
+      <Stack>
+        <Box className="muitech">
+          <CustomFormLabel className="nametech" htmlFor="username">Username</CustomFormLabel>
+          <CustomTextField id="username" placeholder="Username" variant="outlined" fullWidth />
+        </Box>
+        <Box className="muitech">
+          <CustomFormLabel className="nametech" htmlFor="password">Password</CustomFormLabel>
+          <CustomTextField id="password" placeholder="password" type="password" variant="outlined" fullWidth />
+        </Box>
+        <Stack justifyContent="space-between" direction="row" alignItems="center" my={2}>
+          <FormGroup>
+            <FormControlLabel
+              control={<CustomCheckbox defaultChecked />}
+              label="Remeber this Device"
+            />
+          </FormGroup>
+          <Typography
+            component={Link}
+            href="/auth/auth1/forgot-password"
+            fontWeight="500"
+            sx={{
+              textDecoration: 'none',
+              color: 'primary.main',
+            }}
+          >
+            
+          </Typography>
+        </Stack>
       </Stack>
-    </Stack>
-    
-
-    
-    <Box className="muitech"><Typography
-                    component={Link}
-                    href="/auth/auth1/forgot-password"
-                    fontWeight="500"
-                    class="passtech"
-                    sx={{
-                      textDecoration: 'none',
-                      color: 'primary.main',
-                    }}
-                  >
-                    ¿Forgot your password?
-                  </Typography>  
-    </Box>
-    <Box className="muitech">
-
+      
 
       
-      <Button
-        color="primary"
-        variant="contained"
-        size="large"
-        fullWidth
-        component={Link}
-        href="/"
-        type="submit"
-      >
-        Login
-      </Button>
-    </Box>
-    {subtitle}
-  </>
-);
+      <Box className="muitech"><Typography
+                      component={Link}
+                      href="/auth/auth1/forgot-password"
+                      fontWeight="500"
+                      class="passtech"
+                      sx={{
+                        textDecoration: 'none',
+                        color: 'primary.main',
+                      }}
+                    >
+                      ¿Forgot your password?
+                    </Typography>  
+      </Box>
+      <Box className="muitech">
+
+
+        
+        <Button
+          color="primary"
+          variant="contained"
+          size="large"
+          fullWidth
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+      </Box>
+      {subtitle}
+    </>
+  );
+};
 
 export default AuthLogin;
---
+++ src/utils/parse.js
@@ -0,0 +1,8 @@
+// parse.js
+import Parse from 'parse';
+
+// Initialize Parse
+Parse.initialize('NDIFx8hdu3ZLZbB6tUq3au06HmqrhuKkEZ72EVwR', '1MoUVm7jZKt9RR1t1THGN64LQOI7GUu5gvTnQlwZ');
+Parse.serverURL = 'https://parseapi.back4app.com/';
+
+export default Parse;
