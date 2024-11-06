38960e42e91cb8ee42e21ba5890c05ac53b80f8d
Josh <josuedor@gmail.com>
2024-11-06 10:37:44 -0500

    fix(parse): update import statement for Parse module (#5)

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -1,5 +1,5 @@
 // parse.js
-import Parse from 'parse/node';
+import Parse from 'parse';
 
 // Initialize Parse
 Parse.initialize('NDIFx8hdu3ZLZbB6tUq3au06HmqrhuKkEZ72EVwR', '1MoUVm7jZKt9RR1t1THGN64LQOI7GUu5gvTnQlwZ');
