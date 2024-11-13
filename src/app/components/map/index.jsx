6f4daa7fb3d018a32ed3da593f7b327e2e6c8f74
amirsalgado <amirsalgado@gmail.com>
2024-11-13 18:37:11 -0500

    feat(map): integrate LiveQuery for real-time event updates

--- src/app/components/map/index.jsx
+++ src/app/components/map/index.jsx
@@ -1,81 +1,75 @@
 /*Since the map was loaded on client side, 
 we need to make this component client rendered as well*/
-'use client'
+'use client';
 
 import React, { useEffect, useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";//Map component Component from library
-import Parse from "@app/utils/parse";
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'; //Map component Component from library
+import Parse from 'parse ';
 
 const parseClient = new Parse.LiveQueryClient({
-  applicationId: "NDIFx8hdu3ZLZbB6tUq3au06HmqrhuKkEZ72EVwR",
-  serverURL: "wss://safetech.b4a.io", // Asegúrate de usar 'wss://' para conexiones seguras
-  javascriptKey: "1MoUVm7jZKt9RR1t1THGN64LQOI7GUu5gvTnQlwZ"
+  applicationId: 'NDIFx8hdu3ZLZbB6tUq3au06HmqrhuKkEZ72EVwR',
+  serverURL: 'wss://safetech.b4a.io', // Asegúrate de usar 'wss://' para conexiones seguras
+  javascriptKey: '1MoUVm7jZKt9RR1t1THGN64LQOI7GUu5gvTnQlwZ',
 });
 parseClient.open();
 
-
 //Map's styling
 const defaultMapContainerStyle = {
-    width: '100%',
-    height: '100vh',
+  width: '100%',
+  height: '100vh',
 };
 
 //K2's coordinates
 const defaultMapCenter = {
-    lat: 9.3023769,
-    lng: -75.3994999
-}
-
+  lat: 9.3023769,
+  lng: -75.3994999,
+};
 
 //Default zoom level, can be adjusted
-const defaultMapZoom = 17
+const defaultMapZoom = 17;
 
 //Map options
 const defaultMapOptions = {
-    zoomControl: false,
-    tilt: 0,
-    mapTypeControl: false,
-    zoomControl: false,
-    mapTypeControl:false,
-    scaleControl: false,
-    streetViewControl:false,
-    rotateControl:false,
-    fullscreenControl:false,
-    gestureHandling: 'auto',
-    mapTypeId: 'roadmap',
-    styles: [
-      {
-        featureType: "all",
-        elementType: "all",
-        stylers: [
-          { invert_lightness: true }
-        ]
-      },{
-        featureType: "road",
-        elementType: "all",
-        stylers: [
-          { hue: "#00BBC9" }
-        ]
-      },{
-        featureType: "poi",
-        elementType: "all",
-        stylers: [
-          { hue: "#00BBC9" }
-        ]
-      },{
-        featureType: "water",
-        elementType: "all",
-        stylers: [
-          { hue: "#00BBC9" }
-        ]
-      }]
+  zoomControl: false,
+  tilt: 0,
+  mapTypeControl: false,
+  zoomControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+  fullscreenControl: false,
+  gestureHandling: 'auto',
+  mapTypeId: 'roadmap',
+  styles: [
+    {
+      featureType: 'all',
+      elementType: 'all',
+      stylers: [{ invert_lightness: true }],
+    },
+    {
+      featureType: 'road',
+      elementType: 'all',
+      stylers: [{ hue: '#00BBC9' }],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'all',
+      stylers: [{ hue: '#00BBC9' }],
+    },
+    {
+      featureType: 'water',
+      elementType: 'all',
+      stylers: [{ hue: '#00BBC9' }],
+    },
+  ],
 };
 
 const MapComponent = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    const query = new Parse.Query("Event");
+    const query = new Parse.Query('Event');
     const subscription = parseClient.subscribe(query);
 
     subscription.on('open', () => {
@@ -84,14 +78,14 @@ const MapComponent = () => {
 
     subscription.on('create', (event) => {
       console.log('New event:', event);
-      if (event.attributes.type === "commandResult") {        
+      if (event.attributes.type === 'commandResult') {
         setEvents((prevEvents) => [
           ...prevEvents,
           {
             id: event.id,
             lat: event.attributes.latitude,
-            lng: event.attributes.longitude
-          }
+            lng: event.attributes.longitude,
+          },
         ]);
       }
     });
