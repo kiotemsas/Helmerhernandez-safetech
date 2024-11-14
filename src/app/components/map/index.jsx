f09f5bf71529a49bc46b97e53f298be20b699666
Amir Enrique Salgado <amirsalgado@gmail.com>
2024-11-13 19:19:07 -0500

    Feat/maps rendering livequery (#9)
    * feat(map): integrate LiveQuery for real-time event updates
    
    * feat(parse): refactor Parse import and initialize in a separate module
    
    ---------
    
    Co-authored-by: josuedor <josuedor@gmail.com>

--- src/app/components/map/index.jsx
+++ src/app/components/map/index.jsx
@@ -1,96 +1,123 @@
 /*Since the map was loaded on client side, 
 we need to make this component client rendered as well*/
-'use client'
+'use client';
 
-//Map component Component from library
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'; //Map component Component from library
+import Parse from '../../../utils/parse';
+
+const parseClient = new Parse.LiveQueryClient({
+  applicationId: 'NDIFx8hdu3ZLZbB6tUq3au06HmqrhuKkEZ72EVwR',
+  serverURL: 'wss://safetech.b4a.io', // Asegúrate de usar 'wss://' para conexiones seguras
+  javascriptKey: '1MoUVm7jZKt9RR1t1THGN64LQOI7GUu5gvTnQlwZ',
+});
+parseClient.open();
 
 //Map's styling
 const defaultMapContainerStyle = {
-    width: '100%',
-    height: '100vh',
+  width: '100%',
+  height: '100vh',
 };
 
 //K2's coordinates
 const defaultMapCenter = {
-    lat: 9.3023769,
-    lng: -75.3994999
-}
-
+  lat: 9.3023769,
+  lng: -75.3994999,
+};
 
 //Default zoom level, can be adjusted
-const defaultMapZoom = 17
+const defaultMapZoom = 17;
 
 //Map options
 const defaultMapOptions = {
-    zoomControl: false,
-    tilt: 0,
-    mapTypeControl: false,
-    zoomControl: false,
-    mapTypeControl:false,
-    scaleControl: false,
-    streetViewControl:false,
-    rotateControl:false,
-    fullscreenControl:false,
-    gestureHandling: 'auto',
-    mapTypeId: 'roadmap',
-    styles: [
-      {
-        featureType: "all",
-        elementType: "all",
-        stylers: [
-          { invert_lightness: true }
-        ]
-      },{
-        featureType: "road",
-        elementType: "all",
-        stylers: [
-          { hue: "#00BBC9" }
-        ]
-      },{
-        featureType: "poi",
-        elementType: "all",
-        stylers: [
-          { hue: "#00BBC9" }
-        ]
-      },{
-        featureType: "water",
-        elementType: "all",
-        stylers: [
-          { hue: "#00BBC9" }
-        ]
-      }]
+  zoomControl: false,
+  tilt: 0,
+  mapTypeControl: false,
+  zoomControl: false,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+  fullscreenControl: false,
+  gestureHandling: 'auto',
+  mapTypeId: 'roadmap',
+  styles: [
+    {
+      featureType: 'all',
+      elementType: 'all',
+      stylers: [{ invert_lightness: true }],
+    },
+    {
+      featureType: 'road',
+      elementType: 'all',
+      stylers: [{ hue: '#00BBC9' }],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'all',
+      stylers: [{ hue: '#00BBC9' }],
+    },
+    {
+      featureType: 'water',
+      elementType: 'all',
+      stylers: [{ hue: '#00BBC9' }],
+    },
+  ],
 };
-
-
-const location = {
-  lat: 9.3023769,
-  lng: -75.3994999
-};
-
-
 
 const MapComponent = () => {
-    return (
-        <div className="w-full">
-            <GoogleMap
-                mapContainerStyle={defaultMapContainerStyle}
-                center={defaultMapCenter}
-                zoom={defaultMapZoom}
-                options={defaultMapOptions}
-            >
+  const [events, setEvents] = useState([]);
 
-<Marker
+  useEffect(() => {
+    const query = new Parse.Query('Event');
+    const subscription = parseClient.subscribe(query);
 
-options={{
-  icon: '/images/svgs/marker.svg'
-}}
+    subscription.on('open', () => {
+      console.log('LiveQuery connection opened');
+    });
 
-position={location} />
+    subscription.on('create', (event) => {
+      console.log('New event:', event);
+      if (event.attributes.type === 'commandResult') {
+        setEvents((prevEvents) => [
+          ...prevEvents,
+          {
+            id: event.id,
+            lat: event.attributes.latitude,
+            lng: event.attributes.longitude,
+          },
+        ]);
+      }
+    });
 
-            </GoogleMap>
-        </div>
-    )
+    subscription.on('error', (error) => {
+      console.error('LiveQuery error:', error);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+      parseClient.close();
+    };
+  }, []);
+
+  return (
+    <div className="w-full">
+      <GoogleMap
+        mapContainerStyle={defaultMapContainerStyle}
+        center={defaultMapCenter}
+        zoom={defaultMapZoom}
+        options={defaultMapOptions}
+      >
+        {events.map((event) => (
+          <Marker
+            key={event.id}
+            position={{ lat: event.lat, lng: event.lng }}
+            options={{ icon: '/images/svgs/marker.svg' }}
+          />
+        ))}
+      </GoogleMap>
+    </div>
+  );
 };
 
 export { MapComponent };
---
+++ src/utils/parse.js
@@ -0,0 +1,8 @@
+// parse.js
+import Parse from 'parse';
+
+// Initialize Parse
+Parse.initialize('NDIFx8hdu3ZLZbB6tUq3au06HmqrhuKkEZ72EVwR', '1MoUVm7jZKt9RR1t1THGN64LQOI7GUu5gvTnQlwZ');
+Parse.serverURL = 'https://safetech.b4a.io/';
+
+export default Parse;
